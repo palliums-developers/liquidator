de93000fd335229def1088da2ccf472389754134
Xing-Huang <hxg@palliums.org>
2021-01-04 11:49:45 +0800

    change restart

--- restart.sh
+++ restart.sh
@@ -3,7 +3,5 @@
 sudo docker stop liquidator
 sudo docker rm liquidator
 sudo docker image rm liquidator
-sudo docker image build -t liquidator .
-sudo docker run --name=liquidator --rm --network=host -itd liquidator rm ./conf/config.json
-sudo docker stop liquidator
-sudo docker run --name=liquidator --network=host -d liquidator
+sudo docker image build --no-cache -t liquidator .
+sudo docker run --name=liquidator --network=host -itd liquidator
