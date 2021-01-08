091c3e1b3b03d454a7e3cfe07e1171131a232f59
Xing-Huang <hxg@palliums.org>
2021-01-08 17:59:37 +0800

    change update

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker stop liquidator-postgres
-docker rm liquidator-postgres
+sudo docker stop liquidator-postgres
+sudo docker rm liquidator-postgres
 sudo docker run --name=liquidator-postgres -e POSTGRES_PASSWORD="liquidator-postgres" -d -p 5006:5432 postgres
 ./restart.sh
