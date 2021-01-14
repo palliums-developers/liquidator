1e7159b632a859068d2823ff863a7593f64a64d0
Xing-Huang <hxg@palliums.org>
2021-01-14 12:04:25 +0800

    add debug

--- chain_client/__init__.py
+++ chain_client/__init__.py
@@ -49,3 +49,4 @@ class Client:
         return True
 
 
+
--- main.py
+++ main.py
@@ -3,14 +3,15 @@ import json
 from violas_client import Wallet, Client
 from bank import Bank, AccountView, TokenInfo
 import time
-from network import get_liquidator_account
+from network import get_liquidator_account, create_http_client
 
+wallet = Wallet.new()
+a1 = wallet.new_account()
+
+http_client = create_http_client()
+http_client.try_create_child_vasp_account(a1)
 client = Client("violas_testnet")
-currency_code = "vBTC"
+# currency_code = "vBTC"
 account = get_liquidator_account()
-client.mint_coin(account.address, )
-
-index = client.bank_get_currency_index(currency_code)
-token_info = client.get_account_state(client.BANK_OWNER_ADDRESS).get_tokens_resource()
-print(token_info)
+client.transfer_coin(a1, account.address, 100)
 
