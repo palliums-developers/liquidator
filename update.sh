3120eb3b6223619144349f1f5bd21a3e326504c8
Xing-Huang <hxg@palliums.org>
2021-01-18 15:16:18 +0800

    change update

--- update.sh
+++ update.sh
@@ -3,5 +3,12 @@
 sudo docker stop liquidator-postgres
 sudo docker rm liquidator-postgres
 sudo docker run --name=liquidator-postgres -v ${PWD}/create_table.sql:/create_table.sql -e POSTGRES_PASSWORD="liquidator-postgres" -d -p 5006:5432 postgres
+
+sudo docker stop liquidator
+sudo docker rm liquidator
+sudo docker image rm liquidator
+sudo docker image build --no-cache -t liquidator .
+
 sudo docker exec -itd liquidator-postgres psql -U postgres -f /create_table.sql
-./restart.sh
+
+sudo docker run --name=liquidator --network=host -itd liquidator
