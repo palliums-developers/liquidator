d7e3849ba709ab3e23499f70b4127f59d6f1486b
Xing-Huang <hxg@palliums.org>
2021-01-19 17:01:11 +0800

    change back liquidator

--- check_thread.py
+++ check_thread.py
@@ -13,13 +13,13 @@ class CheckerThread(Thread):
     def run(self):
         while True:
             try:
-                time.sleep(60)
                 cur_time = int(time.time() // 60)
                 if cur_time > self.latest_update_time:
                     addrs = self.bank.check_borrow_index(cur_time)
                     for addr in addrs:
                         self.queue.put(addr)
                 self.latest_update_time = cur_time
+                time.sleep(60)
             except Exception as e:
                 import traceback
                 time.sleep(2)
