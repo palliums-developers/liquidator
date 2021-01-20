f0f7766ab0f514e37085a4d7556955d20a75e30d
Xing-Huang <hxg@palliums.org>
2021-01-20 17:34:13 +0800

    add coin porter

--- bank/util.py
+++ bank/util.py
@@ -2,19 +2,19 @@ def new_mantissa(a, b):
     c = a << 64
     d = b << 32
     e = c // d
-    return e
+    return int(e)
 
 
 def mantissa_div(a, b):
     c = a << 32
     d = c // b
-    return d
+    return int(d)
 
 
 def mantissa_mul(a, b):
     c = a * b
     d = c >> 32
-    return d
+    return int(d)
 
 
 def safe_sub(a, b):
