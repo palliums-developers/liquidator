4859d9139a0f0c8c91a3512d6f86ff3e160c166e
Xing-Huang <hxg@palliums.org>
2021-02-01 18:17:19 +0800

    rm debug

--- liquidate_thread.py
+++ liquidate_thread.py
@@ -104,8 +104,8 @@ class LiquidateBorrowThread(Thread):
                 return
 
             liquidate_value = min(bank_value, liquidate_value)
-            liquidate_value = liquidate_value - 1000
-            liquidate_amount = int(mantissa_div(liquidate_value, borrow_currency_price))
+            liquidate_value = liquidate_value
+            liquidate_amount = int(mantissa_div(liquidate_value, borrow_currency_price)*0.9)
 
             '''是否已经注册偿还的币'''
             cs = self.client.get_account_registered_currencies(self.bank_account.address)
