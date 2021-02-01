bc768640f7bbdf919cb5231c23f2088d61d19a6d
Xing-Huang <hxg@palliums.org>
2021-02-01 15:45:59 +0800

    add debug

--- app.py
+++ app.py
@@ -29,9 +29,9 @@ if __name__ == "__main__":
     update_state_thread.setDaemon(True)
     update_state_thread.start()
 
-    liquidator_thread = LiquidateBorrowThread(unhealth_queue)
-    liquidator_thread.setDaemon(True)
-    liquidator_thread.start()
+    # liquidator_thread = LiquidateBorrowThread(unhealth_queue)
+    # liquidator_thread.setDaemon(True)
+    # liquidator_thread.start()
 
     back_liquidator_thread = BackLiquidatorThread()
     back_liquidator_thread.setDaemon(True)
