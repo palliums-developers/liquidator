abaa8b60d93413cd59c3ccbb3bba4c2f05ad7e29
Xing-Huang <hxg@palliums.org>
2021-02-01 15:39:02 +0800

    add debug

--- liquidate_thread.py
+++ liquidate_thread.py
@@ -124,6 +124,7 @@ class LiquidateBorrowThread(Thread):
                 localtime = time.asctime(time.localtime(time.time()))
                 traceback.print_exc()
                 print(localtime, addr, max_borrow_currency, max_lock_currency, liquidate_amount, liquidate_value, mantissa_mul(liquidate_amount, self.bank.get_oracle_price(max_borrow_currency)))
+                print(liquidate_value, self.client.bank_get_total_borrow_value(addr) - self.client.bank_get_total_collateral_value(addr))
             finally:
                 self.coin_porter.add_last_liquidate_id(max_borrow_currency)
                 # print("liquidator_id:", self.coin_porter.last_liquidate_ids)
