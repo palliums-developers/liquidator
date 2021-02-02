0f7bc153e85cf5e4dcc03b87e0cf07fbf6226f2c
Xing-Huang <hxg@palliums.org>
2021-02-02 11:11:59 +0800

    change liquidate

--- main.py
+++ main.py
@@ -7,13 +7,12 @@ from network import get_liquidator_account
 
 
 client = Client("violas_testnet")
-addr = "957f949ca46db1640dd16b50797e82b8"
+addr = "1ae2d48322d9ef0a2d20ae825e44c174"
 # state = client.get_account_state(addr)
 # print(state.get_tokens_resource())
-
-account = get_liquidator_account()
-client.bank_liquidate_borrow(account, addr, "vBTC", "vBTC", 634)
-# print(client.bank_get_total_collateral_value(addr) - client.bank_get_total_borrow_value(addr))
-# print(client.bank_get_total_borrow_value(addr))
+# account = get_liquidator_account()
+# client.bank_liquidate_borrow(account, addr, "vBTC", "vBTC", 634)
+print(client.bank_get_total_collateral_value(addr) - client.bank_get_total_borrow_value(addr))
+print(client.bank_get_total_borrow_value(addr))
 
 
