3a1613ec66a153d9eec1c39941b450a2a3341192
Xing-Huang <hxg@palliums.org>
2021-02-02 10:24:41 +0800

    add check

--- liquidate_thread.py
+++ liquidate_thread.py
@@ -122,11 +122,10 @@ class LiquidateBorrowThread(Thread):
                 if e.args[0] == -32009:
                     time.sleep(1)
                     self.queue.put(addr)
-            except Exception as e:
-                localtime = time.asctime(time.localtime(time.time()))
-                traceback.print_exc()
-                print(localtime, addr, max_borrow_currency, max_lock_currency, liquidate_amount, liquidate_value, mantissa_mul(liquidate_amount, self.bank.get_oracle_price(max_borrow_currency)))
-                time.sleep(1)
+                else:
+                    localtime = time.asctime(time.localtime(time.time()))
+                    traceback.print_exc()
+                    print(localtime, addr, max_borrow_currency, max_lock_currency, liquidate_amount, liquidate_value, mantissa_mul(liquidate_amount, self.bank.get_oracle_price(max_borrow_currency)))
             finally:
                 self.coin_porter.add_last_liquidate_id(max_borrow_currency)
                 # print("liquidator_id:", self.coin_porter.last_liquidate_ids)
